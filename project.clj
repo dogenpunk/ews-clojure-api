84ac3ee825e6c88b878310f8d792ef91ae6688b6
Matthew M. Nelson <dogenpunk@gmail.com>
2020-07-16 15:22:22 -0500

    Add lein-tools-deps plugin
    Move dependencies into deps.edn and add the lein-tools-deps plugin. This
    allows projects using tools.deps to use `:git/url` to import this project.

--- project.clj
+++ project.clj
@@ -2,10 +2,7 @@
   :description "Utility library for accessing Microsoft Exchange"
   :url "https://wwww.bizziapp.com"
 
-  :plugins [[lein-codox "0.10.4"]]
-
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-time "0.14.4"]
-                 [environ "1.1.0"]
-                 [com.microsoft.ews-java-api/ews-java-api "2.0"]
-                 [com.taoensso/timbre "4.10.0"]])
+  :plugins [[lein-codox "0.10.4"]
+            [lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]})
