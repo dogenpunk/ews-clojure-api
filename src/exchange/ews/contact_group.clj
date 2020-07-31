4babe17d5715f68b589c5ff1454e85369f41fb37
Matthew M. Nelson <dogenpunk@gmail.com>
2020-07-30 21:36:36 -0500

    Add multimethod for contact group retrieval

--- src/exchange/ews/contact_group.clj
+++ src/exchange/ews/contact_group.clj
@@ -1,14 +1,34 @@
 (ns exchange.ews.contact-group
-  (:require [exchange.ews.authentication :refer [service-instance]])
+  (:require [exchange.ews.authentication :refer [service-instance]]
+            [clojure.set])
   (:import (microsoft.exchange.webservices.data.core.service.item ContactGroup)
+           (microsoft.exchange.webservices.data.search ItemView)
+           (microsoft.exchange.webservices.data.search.filter SearchFilter$IsEqualTo)
+           (microsoft.exchange.webservices.data.core.service.schema ContactGroupSchema)
+           (microsoft.exchange.webservices.data.core.enumeration.property WellKnownFolderName)
+           (microsoft.exchange.webservices.data.core.enumeration.property EmailAddressKey)
+           (microsoft.exchange.webservices.data.property.complex GroupMember GroupMemberCollection)
            (microsoft.exchange.webservices.data.core.enumeration.service ConflictResolutionMode)
            (microsoft.exchange.webservices.data.property.complex GroupMemberCollection
                                                                  GroupMember
                                                                  ItemId)))
 
-(defn get-contact-group
+(defmulti get-contact-group
+  type)
+
+(defmethod get-contact-group java.lang.String
   [id]
   (ContactGroup/bind @service-instance (ItemId/getItemIdFromString id)))
+
+(defmethod get-contact-group ItemId
+  [id]
+  (ContactGroup/bind @service-instance id))
+
+(defn get-contact-group-by-name
+  ([group-name]
+     (let [sf (SearchFilter$IsEqualTo. ContactGroupSchema/DisplayName group-name)
+           items (.findItems @service-instance WellKnownFolderName/Contacts sf (ItemView. 1))]
+       (first (.getItems items)))))
 
 (defn set-group-display-name
   [id display-name]
