8c4a9535f093ec8014a1c6f60e9346305ed5c338
Matthew M. Nelson <dogenpunk@gmail.com>
2020-07-30 21:35:32 -0500

    Add get-contact-by-name

--- src/exchange/ews/contact.clj
+++ src/exchange/ews/contact.clj
@@ -1,6 +1,12 @@
 (ns exchange.ews.contact
-  (:require [exchange.ews.authentication :refer [service-instance]])
+  (:require [exchange.ews.authentication :refer [service-instance]]
+            [exchange.ews.util :refer [enum-id-cond]])
   (:import (microsoft.exchange.webservices.data.core.service.item Contact)
+           (microsoft.exchange.webservices.data.core.service.schema ContactSchema)
+           (microsoft.exchange.webservices.data.property.complex ItemId)
+           (microsoft.exchange.webservices.data.core.enumeration.property WellKnownFolderName)
+           (microsoft.exchange.webservices.data.search ItemView)
+           (microsoft.exchange.webservices.data.search.filter SearchFilter$IsEqualTo)
            (microsoft.exchange.webservices.data.core.enumeration.service ConflictResolutionMode)
            (microsoft.exchange.webservices.data.property.complex CompleteName
                                                                  EmailAddress
@@ -10,12 +16,24 @@
                                                                  ItemAttachment
                                                                  ItemId
                                                                  PhoneNumberDictionary
-                                                                 PhysicalAddress
                                                                  StringList)))
 
-(defn get-contact
+(defmulti get-contact
+  type)
+
+(defmethod get-contact ItemId
+  [id]
+  (Contact/bind @service-instance id))
+
+(defmethod get-contact java.lang.String
   [id]
   (Contact/bind @service-instance (ItemId/getItemIdFromString id)))
+
+(defn get-contact-by-name
+  [name]
+  (let [search-filter (SearchFilter$IsEqualTo. ContactSchema/DisplayName name)
+        items (.findItems @service-instance WellKnownFolderName/Contacts search-filter (ItemView. 1))]
+    (first (.getItems items))))
 
 (defn set-contact-display-name
   [id display-name]
